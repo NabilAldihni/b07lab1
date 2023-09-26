c22ae451729b8789e6d661a1d66ca6447447c78c
Nabil Aldihni <nabil.aldihni@mail.utoronto.ca>
2023-09-26 12:35:29 -0400

    Updated methods not tested

--- Polynomial.java
+++ Polynomial.java
@@ -2,39 +2,59 @@ import java.lang.Math;
 
 public class Polynomial {
 	public double coefficients[];
+	public int exponents[];
 
 	public Polynomial() {
-		this.coefficients = new double[1];
-		this.coefficients[0] = 0;
+		this.coefficients = new double[0];
 	}
 
-	public Polynomial(double coefficients[]) {
+	public Polynomial(double coefficients[], int exponents[]) {
 		this.coefficients = new double[coefficients.length];
+		this.exponents = new int[exponents.length];
 		for (int i=0; i<coefficients.length; i++) {
 			this.coefficients[i] = coefficients[i];
+			this.exponents[i] = exponents[i];
 		}
 	}
 
 	public Polynomial add(Polynomial poly) {
 		int a_len = this.coefficients.length;
 		int b_len = poly.coefficients.length;
-		int max_len = Math.max(a_len, b_len);
-		int min_len = Math.min(a_len, b_len);
-		double result[] = new double[max_len];
-		for (int i=0; i<min_len; i++) {
-			result[i] = this.coefficients[i] + poly.coefficients[i];
+		double sum[] = new double[a_len + b_len];
+
+		for (int i=0; i<a_len; i++) {
+			sum[this.exponents[i]] += this.coefficients[i];
 		}
-		for (int i=min_len; i<max_len; i++) {
-			result[i] = poly.coefficients[i];
+		for (int i=0; i<b_len; i++) {
+			sum[poly.exponents[i]] += poly.coefficients[i];
 		}
-	
-		return new Polynomial(result);
+
+		int counter = 0;
+		for (int i=0; i<a_len+b_len; i++) {
+			if (sum[i] != 0) {
+				counter++;
+			}
+		}
+
+		double resulting_coefficients[] = new double[counter];
+		int resulting_exponents[] = new int[counter];
+
+		counter = 0;
+		for (int i=0; i<a_len+b_len; i++) {
+			if (sum[i] != 0) {
+				resulting_coefficients[counter] = sum[i];
+				resulting_exponents[counter] = i;
+				counter++;
+			}
+		}
+
+		return new Polynomial(resulting_coefficients, resulting_exponents);
 	}
 
 	public double evaluate(double x) {
 		double result = 0.0;
 		for (int i=0; i<this.coefficients.length; i++) {
-			result += this.coefficients[i] * (Math.pow(x,i));
+			result += this.coefficients[i] * (Math.pow(x,exponents[i]));
 		}
 		
 		return result;
