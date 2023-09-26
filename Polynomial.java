096ba0067df105ae8d4ce266efde55d3d31ed0f6
Nabil Aldihni <nabil.aldihni@mail.utoronto.ca>
2023-09-26 13:04:23 -0400

    No errors, wrong result

--- Polynomial.java
+++ Polynomial.java
@@ -18,18 +18,45 @@ public class Polynomial {
 	}
 
 	public Polynomial add(Polynomial poly) {
-		int exponents1 = this.maxExp(this.exponents);
-		int exponents2 = this.maxExp(poly.exponents);
+		int exponents1 = this.maxExp(this.exponents) + 1;
+		int exponents2 = this.maxExp(poly.exponents) + 1;
 		double poly1[] = new double[exponents1];
 		double poly2[] = new double[exponents2];
 
-		for (int i=0; i<exponents1; i++) {
-			poly1[exponents1[i]] = this.coefficients[i];
+		for (int i=0; i<this.exponents.length; i++) {
+			poly1[this.exponents[i]] = this.coefficients[i];
 		}
-		for (int i=0; i<exponents2; i++) {
-			poly1[exponents2[i]] = poly.coefficients[i];
+		for (int i=0; i<poly.exponents.length; i++) {
+			poly1[poly.exponents[i]] = poly.coefficients[i];
 		}
 
+		double result[] = new double[Math.max(poly1.length, poly2.length)];
+		for (int i=0; i<poly1.length; i++) {
+			result[i] += poly1[i];
+		}
+		for (int i=0; i<poly2.length; i++) {
+			result[i] += poly2[i];
+		}
+
+	 	int non_zeros = 0;
+	 	for (int i=0; i<result.length; i++) {
+			if (result[i] != 0) {
+				non_zeros++;
+			}
+		}
+
+		double co[] = new double[non_zeros];
+		int ex[] = new int[non_zeros];
+		int counter = 0;
+		for (int i=0; i<result.length; i++) {
+			if (result[i] != 0) {
+				ex[counter] = i;
+				co[counter] = result[i];
+				counter++;
+			}
+		}
+
+		return new Polynomial(co, ex);
 
 	}
 
