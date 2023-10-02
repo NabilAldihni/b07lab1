952835bf5e3c33d82f36aa6ddf400c464f33bcc0
NabilAldihni <aldihninabil@gmail.com>
2023-10-01 23:57:20 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -3,28 +3,18 @@ import java.io.IOException;
 
 public class Driver {
 	public static void main(String [] args) {
-		double [] c1 = {7,2,1};
-		int [] e1 = {0,4,9};
-		Polynomial p1 = new Polynomial(c1, e1);
-		double [] c2 = {3,3,5,20};
-		int [] e2 = {1,4,7,11};
-		Polynomial p2 = new Polynomial(c2, e2);
 		String filename = "C:\\Users\\nabil\\Documents\\b07\\b07lab1\\file.txt";
+		String filename2 = "C:\\Users\\nabil\\Documents\\b07\\b07lab1\\file2.txt";
 		try {
-			Polynomial p = new Polynomial(new File(filename));
-			//System.out.println(p);
+			Polynomial p1 = new Polynomial(new File(filename));
+			Polynomial p2 = new Polynomial(new File(filename2));
+			Polynomial s = p1.add(p2);
+			//Polynomial s = p1.multiply(p2);
+			System.out.println(s);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		// Polynomial s = p1.add(p2);
-		// Polynomial s = p1.multiply(p2);
-		// for (double co : s.coefficients) {
-			// System.out.print(co + " ");
-		// }
-		// System.out.println();
-		// for (int ex : s.exponents) {
-			// System.out.print(ex + " ");
-		// }
+		
 	}
 }
